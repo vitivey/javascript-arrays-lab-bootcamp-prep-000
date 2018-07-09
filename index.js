eeda36194ed991788088d4360991f2732174c630
Bluebird Vine <zindzi.thompson@gmail.com>
2018-07-09 19:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Add your functions and code here
+<<<<<<< HEAD
 var kittens= ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
@@ -34,3 +35,24 @@ function removeFirstKitten(){
   return kittens.slice(1)
 }
  
+=======
+var kitten= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kitten.push(name);
+  return kitten
+}
+
+function destructivelyPrependKitten(name){
+  kitten.unshift(name)
+  return kitten
+}
+
+function destructivelyRemoveLastKitten(){
+  return kitten.slice(0, kitten.length - 1)
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kitten.slice(1, kitten.length - 1)
+}
+>>>>>>> dc53deca5a821eecc09e03b1b1ba52e0021d4341
